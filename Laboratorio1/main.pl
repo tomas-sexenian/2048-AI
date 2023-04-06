d7b45bc4cc4f5ffadc8054af988be1490197c829
Matias Rama <matiasrama18@gmail.com>
2023-04-06 14:55:32 -0300

    Initial take on predicates 8 to 10

--- Laboratorio1/main.pl
+++ Laboratorio1/main.pl
@@ -33,3 +33,17 @@ minimo([H|T], C, M) :-
 minimo([H|T], C, M) :-
     H >= C,
     minimo(T, C, M).
+
+repetir(N, V, L) :- repetir(N, V, [], L).
+repetir(0, _, AL, AL).
+repetir(N, V, AL, L) :- N1 is N - 1, repetir(N1, V, [V|AL], L).
+
+matriz(F, C, V, M) :- matriz(F, C, V, [], M).
+matriz(0, _, _, AM, AM).
+matriz(F, C, V, AM, M) :- F1 is F - 1, repetir(C, V, L), matriz(F1, C, V, [L|AM], M).
+
+sustituir(L1, VV, VN, I, C, L2) :- sustituir(L1, VV, VN, I, C, [], L2).
+sustituir([], _, _, _, _, AL2, L2) :- reverse(AL2, L2).
+sustituir([X|L1s], _, _, _, 0, AL2, L2) :- sustituir(L1s, _, _, _, 0, [X|AL2], L2).
+sustituir([VV|L1s], VV, VN, 0, C, AL2, L2) :- C1 is C - 1, sustituir(L1s, VV, VN, 0, C1, [VN|AL2], L2).
+sustituir([X|L1s], VV, VN, I, C, AL2, L2) :-  I1 is I - 1, sustituir(L1s, VV, VN, I1, C, [X|AL2], L2).
