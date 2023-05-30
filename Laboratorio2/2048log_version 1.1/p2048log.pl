4c22f61d02b953538d5c7102ff9afa43e2bfedf5
tomas-sexenian <tsexenian@genexus.com>
2023-05-30 17:47:49 -0300

    a

--- Laboratorio2/2048log_version 1.1/p2048log.pl
+++ Laboratorio2/2048log_version 1.1/p2048log.pl
@@ -1,18 +1,18 @@
-% list of possible moves
+% lista de movimientos posibles
 moves([up, down, left, right]).
 
-% random strategy
+% estrategia aleatoria
 strategy(random, Board, _, Move) :-
 	findall(Move1, (member(Move1, [up, down, left, right]), movimientoT(Board, Move1, NewBoard, _), Board \== NewBoard), Moves),
     (Moves == [] -> Move = up ; random_member(Move, Moves)).
 
-% dummy strategy
+% estrategia dummy
 strategy(dummy, Board, _, Move) :-
     moves(Moves),
     findall(Score-Mv, (member(Mv, Moves), movimientoT(Board, Mv, NewBoard, Score), Board \== NewBoard), ScoresMoves),
     max_member(_-Move, ScoresMoves).
 
-% ai strategy
+% estrategia ia
 strategy(ai, Board, Depth, Move) :-
     findall(Score-Move1, (member(Move1, [up, down, left, right]), calculateScore(Board, Depth, Move1, Score)), ScoresMoves),
     max_member(_-Move, ScoresMoves).
@@ -24,27 +24,27 @@ calculateScore(Board, Depth, Move, Score) :-
 	generateScore(NewBoard, Depth, Score,0),
 	delete_all_cached_values.
 
-% Declare the predicate as dynamic
+% Declarar el predicado como dinámico
 :- dynamic cached_value/2.
 
-% Define a predicate to retrieve the cached value
+% Definir un predicado para recuperar el valor almacenado en caché
 set_cached_value(Key, Value) :-
     % If the value is not cached, compute it and cache it
     assertz(cached_value(Key, Value)).
 
-% Define a predicate to delete a cached value
+% Definir un predicado para eliminar un valor en caché
 delete_cached_value(Key) :-
     % Remove the cached value
     retract(cached_value(Key, _)).
 
-% Define a predicate to delete all cached values
+% Definir un predicado para eliminar todos los valores almacenados en caché
 delete_all_cached_values :-
     % Find all the keys of the cached values
     findall(Key, cached_value(Key, _), Keys),
     % Delete each cached value
     maplist(delete_cached_value, Keys).
 
-% Define a predicate to check if a value is already cached
+% Defina un predicado para verificar si un valor ya está en caché
 is_value_cached(Key, Value) :-
     % Check if the value is already cached
     cached_value(Key, Value), !.
@@ -72,11 +72,11 @@ generateScore(Board, Depth, Score, CleanCache) :-
 	),
 	sum_list(CellsScores, Score).
 
-% multiply/3 multiplies two numbers and returns a floating-point result
+% multiply/3 multiplica dos números y devuelve un resultado de punto flotante
 multiply(X, Y, Result) :-
     Result is float(X) * float(Y).
 
-% simulate2(+Board, +Depth, +Row, +Col, -Score) -> Simulate the score if the cell is filled with 2 (2 has a 80% chance of happening)
+% simulate2(+Board, +Depth, +Row, +Col, -Score) -> Simula el puntaje si la celda se llena con 2 (2 tiene un 80% de posibilidades de suceder)
 simulate2(Board, Depth, Row, Col, Score2, CleanCache) :-
 	arg(Row, Board, RowFunctor),
 	setarg(Col, RowFunctor, 2),
@@ -84,7 +84,7 @@ simulate2(Board, Depth, Row, Col, Score2, CleanCache) :-
 	calculateMoveScore(Board, Depth, MoveScore, CleanCache),
 	multiply(MoveScore, 0.8, Score2).
 
-% simulate4(+Board, +Depth, +Row, +Col, -Score) -> Simulate the score if the cell is filled with 4 (4 has a 20% chance of happening)
+% simulate4(+Board, +Depth, +Row, +Col, -Score) -> Simula el puntaje si la celda se llena con 4 (4 tiene un 20% de probabilidad de suceder)
 simulate4(Board, Depth, Row, Col, Score4, CleanCache) :-
 	arg(Row, Board, RowFunctor),
 	setarg(Col, RowFunctor, 4),
@@ -92,7 +92,7 @@ simulate4(Board, Depth, Row, Col, Score4, CleanCache) :-
 	calculateMoveScore(Board, Depth, MoveScore, CleanCache),
 	multiply(MoveScore, 0.2, Score4).
 
-% calculateMoveScore(+Board, +Depth, -Score) -> Calculate the score of the move
+% calculateMoveScore(+Board, +Depth, -Score) -> Calcula la puntuación de la jugada
 calculateMoveScore(Board, Depth, Score, CleanCache) :-
     (CleanCache == 2 ->  NewLevelCache = 0; NewLevelCache is CleanCache + 1),
 	NextDepth is Depth - 1,
@@ -108,14 +108,14 @@ calculateMoveScore(Board, Depth, Score, CleanCache) :-
 	),
 	max_member(Score, MovesScores).
 
-% edge_score(+Board, -Score) -> This heuristic gives higher scores to tiles placed on the edges of the board.
+% edge_score(+Board, -Score) -> Esta heurística otorga puntuaciones mas altas a las fichas colocadas en los bordes del tablero.
 edge_score(Board, Score) :-
     functor(Board, _, Rows),
     functor(Board, _, Cols),
     findall(Value, (arg(Row, Board, RowFunctor), (Row =:= 1; Row =:= Rows), arg(Col, RowFunctor, Value), (Col =:= 1; Col =:= Cols), (Value == '-' -> Value is 0; true)), EdgeValues),
     sum_list(EdgeValues, Score).
 
-% monotonicity(+Board, -Score) -> This heuristic measures the degree to which the values of the tiles are ordered in a specific direction (e.g., increasing or decreasing)
+% monotonicity(+Board, -Score) -> Esta heurística mide el grado en que los valores de los mosaicos están ordenados en una dirección específica (por ejemplo, aumentando o disminuyendo)
 monotonicity(Board, Score) :-
     findall(RowScore, (arg(_, Board, RowFunctor), row_monotonicity(RowFunctor, RowScore)), RowScores),
     sum_list(RowScores, Score).
@@ -137,7 +137,7 @@ row_monotonicity(Cols, RowFunctor, Score) :-
     row_monotonicity(Cols1, RowFunctor, Score1),
     (X =< Y -> Score is Score1 + 1; Score is Score1).
 
-% smoothness(+Board, -Score) :- This heuristic measures the difference in values between adjacent tiles.
+% smoothness(+Board, -Score) :- Esta heurística mide la diferencia de valores entre mosaicos adyacentes.
 smoothness(Board, Score) :-
     findall(RowScore, (arg(_, Board, RowFunctor), row_smoothness(RowFunctor, RowScore)), RowScores),
     sum_list(RowScores, Score).
@@ -160,17 +160,17 @@ row_smoothness(Cols, RowFunctor, Score) :-
     Diff is abs(X - Y),
     Score is Score1 + Diff.
 
-% max_tile(+Board, -MaxTileScore) -> This heuristic gives higher scores to boards with higher tiles.
+% max_tile(+Board, -MaxTileScore) -> Esta heurística otorga puntajes mas altos a los tableros con mosaicos mas altos.
 max_tile(Board, MaxTileScore) :-
 	findall(Value, (arg(_, Board, RowFunctor), arg(_, RowFunctor, Value), (Value == '-' -> Value is 0; true)), Values),
 	max_list(Values, MaxTileScore).
 
-% count_empty_cells(+Board, -Count) -> This heuristic count the number of empty cells.
+% count_empty_cells(+Board, -Count) -> Esta heurística cuenta el número de celdas vacías.
 count_empty_cells(Board, Count) :-
     findall(1, (arg(_, Board, RowFunctor), arg(_, RowFunctor, Cell), Cell == '-'), Empties),
     length(Empties, Count).
 
-% board evaluation function
+% función de evaluación de la placa
 evaluate_board(Board, Score) :-
 	monotonicity(Board, Monotonicity),
 	smoothness(Board, Smoothness),
@@ -178,7 +178,7 @@ evaluate_board(Board, Score) :-
 	max_tile(Board, MaxTile),
 	Score is (EmptyCells * 256) + (Monotonicity * 128) + (MaxTile * 64) - (Smoothness * 32).
 
-% main predicate for choosing the best move
+% función de evaluación de la placa
 mejor_movimiento(Tablero, NivelMiniMax, Estrategia, Jugada) :-
     strategy(Estrategia, Tablero, NivelMiniMax, Jugada),
     !.
@@ -554,7 +554,7 @@ moverizquierda([X1,X2,X3,X4|X], [N1,N2,N3,N4|N], ScoreGen) :-
 
 %%%%%%%%%%%%%%%%%%%%%%%%% Test %%%%%%%%%%%%%%%%%%%%%%%%%
 
-% testPlayGame plays a game using mejor_movimiento and returns the final score and a flag indicating whether the game was won
+% testPlayGame juega un juego usando mejor_movimiento y devuelve el puntaje final y una bandera que indica si el juego fue ganado
 testPlayGame(Board, NivelMiniMax, Strategy, Won, CurrentScore, FinalScore, FinalBoard) :-
     (game_over(Board) -> 
 		Won = false,
@@ -572,7 +572,7 @@ testPlayGame(Board, NivelMiniMax, Strategy, Won, CurrentScore, FinalScore, Final
 		)
 	).
 
-% Add a new number to the board
+% Añade un nuevo número al tablero
 add_next_number(Grid, NewGrid) :-
     random_between(1, 4, Row),
     random_between(1, 4, Col),
@@ -588,13 +588,13 @@ add_next_number(Grid, NewGrid) :-
 		add_next_number(Grid, NewGrid)
 	).    
 
-% Dummy predicate to check if the game has been won (a 2048 tile has been reached)
+% Predicado dummy para comprobar si se ha ganado el juego (se ha alcanzado una ficha 2048)
 testGameWon(m(f(A, B, C, D),
 			  f(E, F, G, H),
 			  f(I, J, K, L),
 			  f(M, N, O, P))) :- member(2048, [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P]).
 
-% Dummy predicate to check if the game is over (no more moves can be made)
+% Predicado dummy para verificar si el juego ha terminado (no se pueden realizar mas movimientos)
 game_over(Board) :-
 	movimientoT(Board, up, NewBoard, _),
 	Board == NewBoard,
@@ -615,26 +615,22 @@ initial_board(Initial_Board) :-
 	add_next_number(Empty_Board, Board),
 	add_next_number(Board, Initial_Board).
 
-% test/0 generates test data for the game
+% test/0 genera datos de prueba para el juego
 test :- 
 	open('2048_test_data.txt', write, Stream),
 	test(1, Stream, 4),
     close(Stream).
 
-% test/1 generate 10 initial boards and plays 100 games for each board
+% test/3 genera 10 tableros iniciales y juega 100 juegos para cada tablero
 test(11, _, _).
 test(Counter, Stream, NivelMiniMax) :-
 	initial_board(Board),
-	write(Stream,'Testing board: '),
-	write(Stream, Counter),
-	write(Stream, ' '),
-	write(Stream, Board),
 	nl(Stream),
-	testLoop(100, Stream, Board, NivelMiniMax, 'ai'),
+	testLoop(1, Stream, Board, NivelMiniMax, 'ai'),
 	Counter1 is Counter + 1,
 	test(Counter1, Stream, NivelMiniMax).
 
-% testLoop play 100 games with the same initial board and writes the results to a file
+% testLoop juega 100 juegos con el mismo tablero inicial y escribe los resultados en un archivo
 testLoop(101, _, _, _, _).
 testLoop(Counter, Stream, Board, NivelMiniMax, Strategy) :-
     testPlayGame(Board, NivelMiniMax, Strategy, Won, 0, FinalScore, FinalBoard),
@@ -644,21 +640,5 @@ testLoop(Counter, Stream, Board, NivelMiniMax, Strategy) :-
 	write(Stream, ' '),
 	write(Stream, FinalScore),
 	nl(Stream),
-	% Write the final board row by row
-	arg(1, FinalBoard, Row1),
-	arg(2, FinalBoard, Row2),
-	arg(3, FinalBoard, Row3),
-	arg(4, FinalBoard, Row4),
-	write(Stream, Row1),
-	nl(Stream),
-	write(Stream, Row2),
-	nl(Stream),
-	write(Stream, Row3),
-	nl(Stream),
-	write(Stream, Row4),
-	nl(Stream),
     Counter1 is Counter + 1,
     testLoop(Counter1, Stream, Board, NivelMiniMax, Strategy).
-
-% empty_board(m(f('-', '-', '-', '-'),f('-', '-', '-', '-'),f('-', '-', '-', '-'),f('-', '-', '-', '-'))).
-% testGameWon(m(f(2048, '-', '-', '-'),f('-', '-', '-', '-'),f('-', '-', '-', '-'),f('-', '-', '-', '-'))).
